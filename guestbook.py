ca9210553e0a26a58605f8c1db62cc26fb3cae62
Nakada Takumi <usrNeko@dcsyhi.local>
2015-01-29 06:36:14 +0900

    Jinjaをつかった

--- guestbook.py
+++ guestbook.py
@@ -1,29 +1,16 @@
-import cgi
+import os
 import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import jinja2
 import webapp2
 
-MAIN_PAGE_FOOTER_TEMPLATE="""\
-    <form action="/sign?%s" method="post">
-      <div>
-        <textarea name="content" rows="3" cols="60"></textarea>
-      </div>
-      <div>
-        <input type="submit" value="Sign Guestbook">
-      </div>
-    </form>
-    <hr>
-    <form>Guestbook name:
-      <input value="%s" name="guestbook_name">
-      <input type="submit" value="switch">
-    </form>
-    <a href="%s">%s</a>
-  </body>
-</html>
-"""
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
@@ -41,48 +28,46 @@ class Greeting(ndb.Model):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write('<html><body>')
         guestbook_name = self.request.get('guestbook_name',
-                            DEFAULT_GUESTBOOK_NAME)
-        
+                                          DEFAULT_GUESTBOOK_NAME)
         greetings_query = Greeting.query(
-                ancestor = guestbook_key(guestbook_name)).order(-Greeting.date)
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
         user = users.get_current_user()
-        for greeting in greetings:
-            if greeting.author:
-                author = greeting.author.email
-                if user and user.user_id() == greeting.author.identity:
-                    author += '(You)'
-                self.response.write('<b>%s</b> wrote:' % author)
-            else:
-                self.response.write('An anonymous person write:')
-            self.response.write('<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
-        
         if user:
             url = users.create_logout_url(self.request.uri)
-            url_linktext='Logout'
+            url_linktext = 'Logout'
         else:
             url = users.create_login_url(self.request.uri)
-            url_linktext='Login'
+            url_linktext = 'Login'
 
-        sign_query_params = urllib.urlencode({'guestbook_name': guestbook_name})
-        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE % (sign_query_params, cgi.escape(guestbook_name), url, url_linktext))
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
-        guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
         greeting = Greeting(parent=guestbook_key(guestbook_name))
 
         if users.get_current_user():
             greeting.author = Author(
-                    identity = users.get_current_user().user_id(),
-                    email = users.get_current_user().email())
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
+
         greeting.content = self.request.get('content')
         greeting.put()
 
-        query_params = {'guestbook_name' : guestbook_name}
+        query_params = {'guestbook_name': guestbook_name}
         self.redirect('/?' + urllib.urlencode(query_params))
 
 application = webapp2.WSGIApplication([
